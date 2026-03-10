8abfe714d9fd96a287a57de9cd88fbb868ca7427
khaled <Khaledeskandrany@gmail.com>
2026-03-10 15:50:28 +0200

    feat: implement update organization route

--- packages/application/src/organization/routes/index.ts
+++ packages/application/src/organization/routes/index.ts
@@ -1,10 +1,14 @@
 import { organizationListRoute } from "./organization-list.route.js";
 import { createOrganizationRoute } from "../routes/create-organization.route.js";
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { updateOrganizationRoute } from "./update-organization.route.js";
+import { getOrganizationByIdRoute } from "./get-organization.route.js";
 
 export const app = new OpenAPIHono();
 
 app.route("/", createOrganizationRoute);
 app.route("/", organizationListRoute);
+app.route("/", updateOrganizationRoute);
+app.route("/", getOrganizationByIdRoute);
 
 export { app as OrganizationRoutes };
---
+++ packages/application/src/organization/routes/update-organization.route.ts
@@ -0,0 +1,98 @@
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
+import {
+  updateOrganizationBodySchema,
+  mutateOrganizationResponseSchema,
+} from "../schemas.js";
+import {
+  AuthorizationHeaderSchema,
+  createDomainErrorResponseSchema,
+  createResponseSchema,
+  getResourceByIdParamsSchema,
+  globalErrorResponses,
+  ModuleErrorCodes,
+  ModuleErrorResponseMap,
+} from "@avuny/utils";
+
+import { prisma } from "@avuny/db";
+import { getContext, handleResult } from "@avuny/hono";
+import { isAuthenticatedMiddleware } from "../../shared.js";
+import container from "../../container.js";
+
+import { trans } from "../../intl/Translation.js";
+
+export const updateOrganizationRoute = new OpenAPIHono();
+const route = createRoute({
+  method: "post",
+  path: "/",
+  operationId: "updateOrganization",
+  tags: ["organization"],
+  middleware: [isAuthenticatedMiddleware],
+  request: {
+    headers: AuthorizationHeaderSchema,
+    params: getResourceByIdParamsSchema,
+    body: {
+      content: {
+        "application/json": {
+          schema: updateOrganizationBodySchema,
+        },
+      },
+    },
+  },
+
+  responses: {
+    [201]: {
+      description: "Organization have been updated successfully",
+      content: {
+        "application/json": {
+          schema: createResponseSchema(mutateOrganizationResponseSchema),
+        },
+      },
+    },
+    [ModuleErrorResponseMap.MODULE_NAME_CONFLICT.statusCode]: {
+      description: "Organization name is not unique",
+      content: {
+        "application/json": {
+          schema: createDomainErrorResponseSchema([
+            ModuleErrorCodes.MODULE_NAME_CONFLICT,
+          ]),
+        },
+      },
+    },
+
+    [ModuleErrorResponseMap.USER_NO_PERMISSION.statusCode]: {
+      description: "User has no permission to update organization",
+      content: {
+        "application/json": {
+          schema: createDomainErrorResponseSchema([
+            ModuleErrorCodes.USER_NO_PERMISSION,
+          ]),
+        },
+      },
+    },
+    ...globalErrorResponses,
+  },
+});
+
+updateOrganizationRoute.openapi(route, async (c) => {
+  const organizationService = container.resolve("organizationService");
+  const context = getContext(c);
+  const errorTrans = trans({ lang: context.lang as "en" | "ar" });
+  const { id } = c.req.valid("param");
+  const body = c.req.valid("json");
+
+  const result = await organizationService.update({
+    data: body,
+    context,
+    id,
+  });
+  const { MODULE_CREATION_LIMIT_EXCEEDED, ...restModuleErrorResponseMap } =
+    ModuleErrorResponseMap;
+  return handleResult({
+    c,
+    result,
+    successStatus: 201,
+    errorMap: restModuleErrorResponseMap,
+    moduleName: "organization",
+    errorTrans,
+  });
+});
